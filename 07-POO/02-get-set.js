d2b60c83a06e7618bd1defa23bb4e26965841289
RAPHAEL HENRIQUE SANTOS DE OLIVEIRA <26171669@edu119.local>
2026-08-20 16:43:24 -0300

    Salvando Alterações

---
+++ 07-POO/02-get-set.js
@@ -0,0 +1,20 @@
+class Pessoa{
+    #nome;
+
+    constructor(nome){
+        this.#nome = nome;
+    }
+
+    get nome(){
+        return this.#nome
+
+    }
+    set nome(valor){
+        if(valor.lenght >= 2) this.#nome = valor; 
+    }
+
+}
+
+const pessoa = new Pessoa("Pedro");
+pessoa.nome = "Igor";
+console.log(pessoa.nome);
